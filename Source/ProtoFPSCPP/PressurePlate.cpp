6bc57750d6e052afaf7218cd4ccfcce853fd02e0
AlexkyTD <alexisveillon@hotmail.fr>
2020-03-30 17:17:26 +0200

    PressurePlate
    g fé la pressure plate

--- Source/ProtoFPSCPP/PressurePlate.cpp
+++ Source/ProtoFPSCPP/PressurePlate.cpp
@@ -1,7 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PressurePlate.h"
+#include "Components/StaticMeshComponent.h"
+#include "Materials/MaterialInstanceDynamic.h"
+#include "ConstructorHelpers.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Engine/StaticMesh.h"
+#include "Components/PrimitiveComponent.h"
 
 // Sets default values
 APressurePlate::APressurePlate()
@@ -9,12 +15,26 @@ APressurePlate::APressurePlate()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("mesh"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>meshRef(TEXT("/Game/Geometry/Meshes/1M_Cube"));
+	SetRootComponent(mesh);
+	mesh->SetStaticMesh(meshRef.Object);
+	static ConstructorHelpers::FObjectFinder<UMaterialInstance>mat(TEXT("/Game/Materials/Mat_Pillar"));
+	mesh->SetMaterial(0, mat.Object);
+	box = CreateDefaultSubobject<UBoxComponent>(TEXT("box"));
+	box->SetupAttachment(mesh);
+	box->SetBoxExtent(mesh->GetStaticMesh()->GetBoundingBox().GetExtent());
+	box->SetRelativeLocation(FVector(0.f, 0.f, 20.f));
+	box->OnComponentBeginOverlap.AddDynamic(this, &APressurePlate::BoxBeginOverlap);
+
+
 }
 
 // Called when the game starts or when spawned
 void APressurePlate::BeginPlay()
 {
 	Super::BeginPlay();
+	FVector ActorLocation = GetActorLocation();
 	
 }
 
@@ -22,6 +42,17 @@ void APressurePlate::BeginPlay()
 void APressurePlate::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	
+	if (overlap && mesh->GetRelativeTransform().GetLocation().Z < -20.f) {
+		FVector ActorLocation = mesh->GetRelativeTransform().GetLocation();
+		mesh->SetRelativeLocation(GetActorLocation()-FVector(0.f, 0.f, -1.f*DeltaTime));
+	}
 }
+
+void APressurePlate::BoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	overlap = true;
+}
+
+
 
--- Source/ProtoFPSCPP/PressurePlate.h
+++ Source/ProtoFPSCPP/PressurePlate.h
@@ -15,12 +15,25 @@ public:
 	// Sets default values for this actor's properties
 	APressurePlate();
 
+	UPROPERTY(EditAnywhere)
+		class UStaticMeshComponent* mesh;
+
+	UPROPERTY(EditAnywhere)
+		class UBoxComponent* box;
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	bool overlap = false;
+
+	
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void BoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	
 };
